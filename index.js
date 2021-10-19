667c2ca79a8e5542ba31bb59e30930bf9dc5d0e2
Kaido24 <mumm.meelis@gmail.com>
2021-10-19 22:40:53 +0300

    refactoring

--- index.js
+++ index.js
@@ -12,80 +12,93 @@ async function test() {
   driver.manage().window().maximize();
 
   //opens webpage
-  await driver.get('https://www.weekendshoes.ee');
-  console.log(chalk.green(`1. Opened front page`));
+  await driver.get('https://www.weekendshoes.ee').then(console.log(chalk.green('1. Opened front page')));
 
   //logs webpage's title
   const pageTitle = await driver.getTitle();
   console.log(chalk.cyan(`Webpage title: ${pageTitle}`));
 
   //opens up women's shoes
-  await driver.get('https://www.weekendshoes.ee/naistele/saapad.html');
-  console.log(chalk.green(`2. Opened women's shoes`));
+  await driver.get('https://www.weekendshoes.ee/naistele/saapad.html').then(console.log(chalk.green("2. Opened women's shoes")));
 
   //adds item to wishlist
   await driver.manage().setTimeouts({ implicit: 10000 });
   const itemElement = await driver.findElement(By.xpath('//*[@id="amasty-shopby-product-list"]/div[3]/ol/li[6]/div/div[3]/div/div/a'));
-  driver.executeScript('arguments[0].click();', itemElement);
-  console.log(chalk.green('3. Add product to wishlist'));
+  driver.executeScript('arguments[0].click();', itemElement).then(console.log(chalk.green('3. Added item to wishlist')));
 
   loadingCheck();
   // finds wishlist
-  await driver.findElement(By.id('wishlist-link')).click();
-  console.log(chalk.green('4. Found wishlist'));
+  await driver
+    .findElement(By.id('wishlist-link'))
+    .click()
+    .then(console.log(chalk.green('4. Found wishlist')));
 
   // opens wishlist
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.xpath('//*[@id="miniwishlist-content-wrapper"]/div/div/div/button')).click();
-  console.log(chalk.green('5. Opened wishlist'));
+  await driver
+    .findElement(By.xpath('//*[@id="miniwishlist-content-wrapper"]/div/div/div/button'))
+    .click()
+    .then(console.log(chalk.green('5. Opened wishlist')));
 
   // opens up item page
   await driver.manage().setTimeouts({ implicit: 10000 });
   const wishlistItem = await driver.findElement(By.className('product-item-link'));
-  driver.executeScript('arguments[0].click();', wishlistItem);
-  console.log(chalk.green('6. Opened item'));
+  driver.executeScript('arguments[0].click();', wishlistItem).then(console.log(chalk.green('6. Opened item page')));
 
   // picks size from drop-down menu
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.className('selectric-wrapper')).click();
-  console.log(chalk.green('7. Opened drop-down menu'));
+  await driver
+    .findElement(By.className('selectric-wrapper'))
+    .click()
+    .then(console.log(chalk.green('7. Opened drop-down menu')));
 
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.xpath('//*[@id="product-options-wrapper"]/div/div/div/div/div[3]/div/ul/li[3]')).click();
-  console.log(chalk.green('8. Picked drop-down menu value'));
+  await driver
+    .findElement(By.xpath('//*[@id="product-options-wrapper"]/div/div/div/div/div[3]/div/ul/li[3]'))
+    .click()
+    .then(console.log(chalk.green('8. Picked drop-down menu value')));
 
   // adds to shopping cart
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.id('product-addtocart-button')).click();
-  console.log(chalk.green('9. Added to shopping cart'));
+  await driver
+    .findElement(By.id('product-addtocart-button'))
+    .click()
+    .then(console.log(chalk.green('9. Added to shopping cart')));
 
   // opens shopping cart
   await driver.manage().setTimeouts({ implicit: 10000 });
   const shoppingCart = await driver.findElement(By.xpath('//*[@id="minicart-content-wrapper"]/div[2]/div[4]/div/a'));
-  driver.executeScript('arguments[0].click();', shoppingCart);
-  console.log(chalk.green('10. Opened shopping cart'));
+  driver.executeScript('arguments[0].click();', shoppingCart).then(console.log(chalk.green('10. Opened shopping cart')));
 
   // increases quantity by 1
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.className('increase-qty')).click();
-  console.log(chalk.green('11. Increased quantity by 1'));
+  await driver
+    .findElement(By.className('increase-qty'))
+    .click()
+    .then(console.log(chalk.green('11. Increased quantity by 1')));
 
   loadingCheck();
 
   // removes item
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.xpath('//*[@id="shopping-cart-table"]/tbody/tr[1]/td[6]/a')).click();
-  console.log(chalk.green('12. Removed all items'));
+  await driver
+    .findElement(By.xpath('//*[@id="shopping-cart-table"]/tbody/tr[1]/td[6]/a'))
+    .click()
+    .then(console.log(chalk.green('12. Removed items')));
 
   // searches for searchString
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.id('search')).sendKeys(searchString, Key.RETURN);
-  console.log(chalk.green('13. Used search'));
+  await driver
+    .findElement(By.id('search'))
+    .sendKeys(searchString, Key.RETURN)
+    .then(console.log(chalk.green('13. Used search')));
 
   // sorts by popularity
   await driver.manage().setTimeouts({ implicit: 10000 });
-  await driver.findElement(By.css('#sorter [value="bestsellers"]')).click();
-  console.log(chalk.green('14. Sorted by popularity'));
+  await driver
+    .findElement(By.css('#sorter [value="bestsellers"]'))
+    .click()
+    .then(console.log(chalk.green('14. Sorted by popularity')));
   console.log(chalk.yellow('TEST COMPLETED'));
 
   // checks if page is loading
